6fb43bde6dd65e8a0f4f601af31418c49844467f
Thomas Tran <tran0@seas.upenn.edu>
2024-08-07 17:36:01 -0500

    Updated implementation. Calculate avg run time

--- matrix_inversion.cu
+++ matrix_inversion.cu
@@ -1,40 +1,32 @@
 #include <iostream>
 #include <cuda_runtime.h>
-#include <curand.h>
-#include <curand_kernel.h>
 #include <cusolverDn.h>
+#include <iomanip> 
 
-#define MATRIX_DIM 300
+#define MATRIX_DIM 3
+#define ITER 100
 
-__global__ void initializeMatrix(float* d_matrix, int matrixDim, int seed)
+void createMatrix(float* h_matrix)
 {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    int idy = threadIdx.y + blockIdx.y * blockDim.y;
-    int index = idy * matrixDim + idx;
-
-    // Initialize matrix with random int from 0-100
-    if (idx < matrixDim && idy < matrixDim)
+    for(int i = 0; i < MATRIX_DIM * MATRIX_DIM; i++)
     {
-        curandState state;
-        curand_init(seed, index, 0, &state);
-        d_matrix[index] = static_cast<float>(curand(&state) % 100);
+        h_matrix[i] = (rand()) % 100;
     }
 }
 
-__global__ void setIdentityMatrix(float* matrix, int n) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < n * n) {
-        int row = idx / n;
-        int col = idx % n;
-        matrix[idx] = (row == col) ? 1.0f : 0.0f;
-    }
-}
-
-
-void printMatrix(const float* matrix, int size) {
+void setIdentityMatrix(float* matrix, int size) {
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
-            std::cout << matrix[i * size + j] << " ";
+            matrix[i*size+j] = (i == j) ? 1.0f : 0.0f;
+        }
+    }
+}
+
+
+void printMatrix(const float* matrix) {
+    for (int i = 0; i < MATRIX_DIM; ++i) {
+        for (int j = 0; j < MATRIX_DIM; ++j) {
+            std::cout << matrix[i * MATRIX_DIM + j] << " ";
         }
         std::cout << std::endl;
     }
@@ -43,101 +35,90 @@ void printMatrix(const float* matrix, int size) {
 int main()
 {
     const int matrixSize = MATRIX_DIM * MATRIX_DIM * sizeof(float);
-
-    //Host
-    float h_matrix[MATRIX_DIM*MATRIX_DIM];
-    float h_invMatrix[MATRIX_DIM*MATRIX_DIM];
-    
-    // Device
-    float* d_matrix;
-    float* d_invMatrix;
-    int* d_info;
-    cudaMalloc(&d_matrix, matrixSize);
-    cudaMalloc(&d_invMatrix, matrixSize);
-    cudaMalloc(&d_info, sizeof(int));
-
-    int threadsPerBlock = 16; 
-    dim3 blockSize(threadsPerBlock, threadsPerBlock);
-    dim3 gridSize((MATRIX_DIM + threadsPerBlock - 1) / threadsPerBlock, 
-                (MATRIX_DIM + threadsPerBlock - 1) / threadsPerBlock);
-
-
-    cudaEvent_t start, stop;
-    float computation_time;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start,0);
-
-    initializeMatrix<<<gridSize, blockSize>>>(d_matrix, MATRIX_DIM, time(NULL));
-    cudaMemcpy(h_matrix, d_matrix, matrixSize, cudaMemcpyDeviceToHost);
-
-    std::cout << "Current matrix:" << std::endl;
-    //printMatrix((float*)h_matrix, MATRIX_DIM);
-
-    cusolverDnHandle_t cussolverHandle;
-    cusolverDnCreate(&cussolverHandle);
-    
-    // Workspace size query
-    int workspace_size = 0;
-    cusolverDnSgetrf_bufferSize(cussolverHandle, MATRIX_DIM, MATRIX_DIM, d_matrix, MATRIX_DIM, &workspace_size);
-    size_t free, total;
-    cudaMemGetInfo(&free, &total);
-    std::cout << "GPU memory: " << free / (1024*1024) << "MB free of " << total / (1024*1024) << "MB total" << std::endl;
-    std::cout << "Workspace size:" << workspace_size << std::endl;
-    std::cout << "Workspace size: " << workspace_size * sizeof(float) / (1024*1024) << " MB" << std::endl;
-
-    float* d_work;
-    cudaMalloc(&d_work, workspace_size * sizeof(float));
-
-    // LU factorization
-    int* d_ipiv;
-    cudaMalloc(&d_ipiv, MATRIX_DIM * sizeof(int));
-    cusolverDnSgetrf(cussolverHandle, MATRIX_DIM, MATRIX_DIM, d_matrix, MATRIX_DIM, d_work, d_ipiv, d_info);
-
-    // Copy LU matrix back to host
-    cudaMemcpy(h_matrix, d_matrix, matrixSize, cudaMemcpyDeviceToHost);
-    std::cout << "LU matrix:" << std::endl;
-    //printMatrix((float*)h_matrix, MATRIX_DIM);
-
-    int h_info[1];
-    cudaMemcpy(h_info,d_info,1*sizeof(int),cudaMemcpyDeviceToHost);
-
-    for (int i = 0; i < 1; ++i)
+    float res[ITER];
+	float *curr = res;
+    for (int i = 0; i < ITER; ++i)
     {
-        std::cout << "h_info: " << i << " = " << h_info[i] << std::endl;
+        //Host
+        float h_matrix[MATRIX_DIM*MATRIX_DIM];
+        float h_invMatrix[MATRIX_DIM*MATRIX_DIM];
+        createMatrix(h_matrix);
+        //printMatrix(h_matrix);
+        
+        // Device
+        float* d_matrix;
+        float* d_invMatrix;
+        float* d_identity;
+        int* d_info;
+        float* d_work;
+        int* d_ipiv;
+        cudaMalloc(&d_matrix, matrixSize);
+        cudaMalloc(&d_invMatrix, matrixSize);
+        cudaMalloc(&d_identity, matrixSize);
+        cudaMalloc(&d_info, sizeof(int));
+        cudaMalloc(&d_work, workspace_size * sizeof(float));
+        cudaMalloc(&d_ipiv, MATRIX_DIM * sizeof(int));
+        cudaMemcpy(d_matrix, h_matrix, matrixSize, cudaMemcpyHostToDevice);
+
+
+        cudaEvent_t start, stop;
+        float computation_time;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        cudaEventRecord(start,0);
+        
+
+        cusolverDnHandle_t cussolverHandle;
+        cusolverDnCreate(&cussolverHandle);
+
+        // Workspace size query
+        int workspace_size = 0;
+        cusolverDnSgetrf_bufferSize(cussolverHandle, MATRIX_DIM, MATRIX_DIM, d_matrix, MATRIX_DIM, &workspace_size);
+
+        // LU factorization
+        cusolverDnSgetrf(cussolverHandle, MATRIX_DIM, MATRIX_DIM, d_matrix, MATRIX_DIM, d_work, d_ipiv, d_info);
+        cudaDeviceSynchronize();
+
+        // Identity matrix
+        float h_identity[MATRIX_DIM*MATRIX_DIM];
+        setIdentityMatrix(h_identity, MATRIX_DIM);
+
+        cudaMemcpy(d_identity, h_identity, matrixSize, cudaMemcpyHostToDevice);
+
+        // Invert the matrix
+        cusolverDnSgetrs(cussolverHandle, CUBLAS_OP_N, MATRIX_DIM, MATRIX_DIM, d_matrix, MATRIX_DIM, d_ipiv, d_identity, MATRIX_DIM, d_info);
+        cudaDeviceSynchronize();
+        cudaMemcpy(h_invMatrix, d_identity, matrixSize, cudaMemcpyDeviceToHost);
+
+        cudaEventRecord(stop,0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&computation_time, start, stop);
+
+        *curr = computation_time;
+        ++curr;
+        
+        std::cout<< std::fixed << std::setprecision(5);
+        std::cout<< "Computation took " << computation_time << "ms" << std::endl;
+        //std::cout<< "Inverted Matrix:"<< std::endl;
+        //std::cout<< h_invMatrix << std::endl;
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+        cudaFree(d_matrix);
+        cudaFree(d_invMatrix);
+        cudaFree(d_identity);
+        cudaFree(d_info);
+        cudaFree(d_work);
+        cudaFree(d_ipiv);
+        cusolverDnDestroy(cussolverHandle);
+
     }
 
-    // Identity matrix
-    float* d_identity;
-    cudaMalloc(&d_identity, matrixSize);
-    cudaMemset(d_identity, 0, matrixSize);
-    int threadsPerBlock1D = 256;
-    int numBlocks1D = (MATRIX_DIM * MATRIX_DIM + threadsPerBlock1D - 1) / threadsPerBlock1D;
-    setIdentityMatrix<<<numBlocks1D, threadsPerBlock1D>>>(d_identity, MATRIX_DIM);
+    float total = 0.0;
 
-    // Invert the matrix
-    cusolverDnSgetrs(cussolverHandle, CUBLAS_OP_N, MATRIX_DIM, MATRIX_DIM, d_matrix, MATRIX_DIM, d_ipiv, d_identity, MATRIX_DIM, d_info);
-
-    // Copy inverted matrix back to host
-    cudaMemcpy(h_invMatrix, d_identity, matrixSize, cudaMemcpyDeviceToHost);
-    cudaEventRecord(stop,0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&computation_time, start, stop);
-
-    printf("Inverted Matrix:\n");
-    //printMatrix(h_invMatrix, MATRIX_DIM);
-    printf("Computation took %.10fms\n", computation_time);
-
-    cudaEventDestroy(start);
-    cudaEventDestroy(stop);
-
-
-    cudaFree(d_matrix);
-    cudaFree(d_invMatrix);
-    cudaFree(d_identity);
-    cudaFree(d_info);
-    cudaFree(d_work);
-    cudaFree(d_ipiv);
-    cusolverDnDestroy(cussolverHandle);
+	for (int x = 0; x < ITER; ++x)
+	{
+		total += res[x];
+	}
+	std::cout << "avg Cuda Time: " << total/ITER << "ms\n";\
 
 }
